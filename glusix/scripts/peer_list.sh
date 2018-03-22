28993c1c85210e5ff48208c2a700b971630865a9
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-22 15:20:32 +0100

    Updating peer_list scripts.

--- glusix/scripts/peer_list.sh
+++ glusix/scripts/peer_list.sh
@@ -5,6 +5,6 @@ GLUSTER=`which gluster`
 ${GLUSTER} pool list 2>/dev/null|egrep -v "(^UUID|^$)" | while read line; do
    id=`echo ${line}|awk '{print $1}'|awk '{$1=$1};1'`
    host=`echo ${line}|awk '{print $2}'|awk '{$1=$1};1'`
-   state=`echo ${line}|awk '{print $2}'|awk '{$1=$1};1'`
+   state=`echo ${line}|awk '{print $3}'|awk '{$1=$1};1'`
    echo "${id}|${host}|${state}"
 done
