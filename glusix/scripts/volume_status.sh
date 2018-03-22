43c134c0c531ea175a00a12f981aacbed4ce8890
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-22 14:51:37 +0100

    Updating volume_status scripts.

--- glusix/scripts/volume_status.sh
+++ glusix/scripts/volume_status.sh
@@ -23,15 +23,15 @@ if [[ ${ATTR} =~ (bricks|inode|size) ]]; then
 	    res=`echo "${output}" | grep -e "^Inode Count" | \
            awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
 	elif [[ ${PARAM1} == 'free' ]]; then
-	    if [[ -z ${PARAM2} ]]; then
-		res=`echo "${output}" | grep -e "^Free Inodes" | \
-              awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-	    elif [[ ${PARAM2} == 'perc' ]]; then
+	    if [[ ${PARAM2} == 'perc' ]]; then
 		total=`echo "${output}" | grep -e "^Inode Count" | \
-		awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+		       awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
 		free=`echo "${output}" | grep -e "^Free Inodes" | \
-               awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+               	      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
 		res=`echo $(( (${free}*100) / ${total}))`
+	    else
+		res=`echo "${output}" | grep -e "^Free Inodes" | \
+        	     awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
 	    fi
 	fi
     fi
@@ -46,6 +46,15 @@ elif [[ ${ATTR} == 'clients' ]]; then
 	raw=`echo "${output}"|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
 	raw=`echo "${raw}"|awk -F: '{print $1}'|awk '{$1=$1};1'`
 	res=`echo "${raw}"|sort|uniq|wc -l`
+    elif [[ ${PARAM1} == "bytes" ]]; then
+	raw=`echo "${output}"|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+	if [[ ${PARAM2} == 'read' ]]; then
+	    raw=`echo "${raw}"|awk '{print $2}'|awk '{$1=$1};1'`
+	    res=`echo "${raw:-0}"|paste -sd+ -|bc`
+	elif [[ ${PARAM2} == 'written' ]]; then
+	    raw=`echo "${raw}"|awk '{print $3}'|awk '{$1=$1};1'`
+	    res=`echo "${raw:-0}"|paste -sd+ -|bc`
+	fi
     fi
 fi
 echo "${res:-0}"
