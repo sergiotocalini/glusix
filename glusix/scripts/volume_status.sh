09c050303674ab50f7fa0f9d88789b8e0712d2ba
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-22 14:35:41 +0100

    Updating volume_status scripts.

--- glusix/scripts/volume_status.sh
+++ glusix/scripts/volume_status.sh
@@ -6,32 +6,42 @@ ATTR="${2:-bricks}"
 PARAM1="${3}"
 PARAM2="${4}"
 
-output=`${GLUSTER} volume status ${VOLNAME} detail 2>/dev/null`
-if [[ ${ATTR} == 'bricks' ]]; then
-   all=`echo "${output}" | grep -e "^Online" | wc -l`
-   sum=`echo "${output}" | grep -e "^Online" | grep ": Y" | wc -l`
-   if [[ ${PARAM1:-quorum} == 'quorum' ]]; then
-      res=`echo $(( (${sum}*100) / ${all}))`
-   elif [[ ${PARAM1} == 'online' ]]; then
-      res=${sum}
-   elif [[ ${PARAM1} == 'total' ]]; then
-      res=${all}
-   fi
-elif [[ ${ATTR} == 'inode' ]]; then
-   if [[ ${PARAM1:-total} == 'total' ]]; then
-      res=`echo "${output}" | grep -e "^Inode Count" | \
+if [[ ${ATTR} =~ (bricks|inode|size) ]]; then
+    output=`${GLUSTER} volume status ${VOLNAME} detail 2>/dev/null`
+    if [[ ${ATTR} == 'bricks' ]]; then
+	all=`echo "${output}" | grep -e "^Online" | wc -l`
+	sum=`echo "${output}" | grep -e "^Online" | grep ": Y" | wc -l`
+	if [[ ${PARAM1:-quorum} == 'quorum' ]]; then
+	    res=`echo $(( (${sum}*100) / ${all}))`
+	elif [[ ${PARAM1} == 'online' ]]; then
+	    res=${sum}
+	elif [[ ${PARAM1} == 'total' ]]; then
+	    res=${all}
+	fi
+    elif [[ ${ATTR} == 'inode' ]]; then
+	if [[ ${PARAM1:-total} == 'total' ]]; then
+	    res=`echo "${output}" | grep -e "^Inode Count" | \
            awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-   elif [[ ${PARAM1} == 'free' ]]; then
-      if [[ -z ${PARAM2} ]]; then
-         res=`echo "${output}" | grep -e "^Free Inodes" | \
+	elif [[ ${PARAM1} == 'free' ]]; then
+	    if [[ -z ${PARAM2} ]]; then
+		res=`echo "${output}" | grep -e "^Free Inodes" | \
               awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-      elif [[ ${PARAM2} == 'perc' ]]; then
-         total=`echo "${output}" | grep -e "^Inode Count" | \
+	    elif [[ ${PARAM2} == 'perc' ]]; then
+		total=`echo "${output}" | grep -e "^Inode Count" | \
 		awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-         free=`echo "${output}" | grep -e "^Free Inodes" | \
+		free=`echo "${output}" | grep -e "^Free Inodes" | \
                awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-         res=`echo $(( (${free}*100) / ${total}))`
-      fi
-   fi
+		res=`echo $(( (${free}*100) / ${total}))`
+	    fi
+	fi
+    fi
+elif [[ ${ATTR} == 'clients' ]]; then
+    output=`${GLUSTER} volume status ${VOLNAME} clients 2>/dev/null`
+    if [[ ${PARAM1:-avg} == "avg" ]]; then
+	raw=`echo "${output}" | grep -e "^Client connected : " | awk '{print $2}' | awk '{$1=$1};1'`
+	all=`echo "${raw:-0}" | wc -l`
+	sum=`echo "${raw:-0}" | paste -sd+ - | bc`
+	res=`echo $(( ${sum:-0} / ${all} ))`
+    fi
 fi
 echo "${res:-0}"
