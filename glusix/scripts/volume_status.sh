bfe204bd96169d5a890ea5b93e0cffff03f824c7
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-23 13:16:09 +0100

    Updating volume_status script.

--- glusix/scripts/volume_status.sh
+++ glusix/scripts/volume_status.sh
@@ -6,60 +6,75 @@ ATTR="${2:-bricks}"
 PARAM1="${3}"
 PARAM2="${4}"
 
-function display_units() {
+display_units() {
     value="${1:-1}"
-    units="${2:-B}"
-    type="${3:-dec}"
-    
-    available
-    
+    to_units="${2:-B}"
+    divide="${3:-1024}"
+
+    from_val=`echo ${value} | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{$1=$1};1'`
+    from_units=`echo ${value} | sed "s/${from_val}//"`
+
+    if [[ ${to_units} == ${from_units:-B} ]]; then
+        res="${from_val}"
+    else
+        if [[ ${from_units} == 'TB' && ${to_units} == 'B' ]]; then
+            ec="${from_val} * (${divide} ^ 4)"
+        elif [[ ${from_units} == 'GB' && ${to_units} == 'B' ]]; then
+            ec="${from_val} * (${divide} ^ 3)"
+        elif [[ ${from_units} == 'MB' && ${to_units} == 'B' ]]; then
+            ec="${from_val} * (${divide} ^ 2)"
+        fi
+        res=`printf "%.0f" $(echo "${ec}" | bc)`
+    fi
+    echo "${res:-0}"
+    return 0
 }
 
 if [[ ${ATTR} =~ (bricks|inode|size) ]]; then
     output=`${GLUSTER} volume status ${VOLNAME} detail 2>/dev/null`
     if [[ ${ATTR} == 'bricks' ]]; then
-	all=`echo "${output}" | grep -e "^Online" | wc -l`
-	sum=`echo "${output}" | grep -e "^Online" | grep ": Y" | wc -l`
-	if [[ ${PARAM1:-quorum} == 'quorum' ]]; then
-	    res=`echo $(( (${sum}*100) / ${all}))`
-	elif [[ ${PARAM1} == 'online' ]]; then
-	    res=${sum}
-	elif [[ ${PARAM1} == 'total' ]]; then
-	    res=${all}
-	fi
+        all=`echo "${output}" | grep -e "^Online" | wc -l`
+        sum=`echo "${output}" | grep -e "^Online" | grep ": Y" | wc -l`
+        if [[ ${PARAM1:-quorum} == 'quorum' ]]; then
+            res=`echo $(( (${sum}*100) / ${all}))`
+        elif [[ ${PARAM1} == 'online' ]]; then
+            res=${sum}
+        elif [[ ${PARAM1} == 'total' ]]; then
+            res=${all}
+        fi
     elif [[ ${ATTR} == 'inodes' ]]; then
-	if [[ ${PARAM1:-total} == 'total' ]]; then
-	    res=`echo "${output}" | grep -e "^Inode Count" | \
-           awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-	elif [[ ${PARAM1} == 'free' ]]; then
-	    if [[ ${PARAM2} == 'perc' ]]; then
-		total=`echo "${output}" | grep -e "^Inode Count" | \
-		       awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-		free=`echo "${output}" | grep -e "^Free Inodes" | \
-               	      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-		res=`echo $(( (${free}*100) / ${total}))`
-	    else
-		res=`echo "${output}" | grep -e "^Free Inodes" | \
-        	     awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-	    fi
-	fi
-    elif [[ ${ATTR} == 'size' ]]; then
-	if [[ ${PARAM1:-total} == 'total' ]]; then
-	    raw=`echo "${output}" | grep -e "^Total Disk Space" | \
-	         awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-	elif [[ ${PARAM1} == 'free' ]]; then
-	    if [[ ${PARAM2} == 'perc' ]]; then
-		total=`echo "${output}" | grep -e "^Total Disk Space" | \
-		       awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-		free=`echo "${output}" | grep -e "^Disk Space Free" | \
-               	      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-		raw=`echo $(( ($( display_units ${free} )*100) / $( display_units "${total}" )) ))`
-	    else
-		raw=`echo "${output}" | grep -e "^Disk Space Free" | \
+        if [[ ${PARAM1:-total} == 'total' ]]; then
+            res=`echo "${output}" | grep -e "^Inode Count" | \
+                 awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+        elif [[ ${PARAM1} == 'free' ]]; then
+            if [[ ${PARAM2} == 'perc' ]]; then
+                total=`echo "${output}" | grep -e "^Inode Count" | \
+                       awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+                free=`echo "${output}" | grep -e "^Free Inodes" | \
+                      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+                res=`echo $(( (${free}*100) / ${total}))`
+            else
+                res=`echo "${output}" | grep -e "^Free Inodes" | \
                      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
-	    fi
-	fi
-	res=$( display_units "${raw}" )
+            fi
+        fi
+    elif [[ ${ATTR} == 'size' ]]; then
+        if [[ ${PARAM1:-total} == 'total' ]]; then
+            raw=`echo "${output}" | grep -e "^Total Disk Space" | \
+                 awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+        elif [[ ${PARAM1} == 'free' ]]; then
+            if [[ ${PARAM2} == 'perc' ]]; then
+                total=`echo "${output}" | grep -e "^Total Disk Space" | \
+                       awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+                free=`echo "${output}" | grep -e "^Disk Space Free" | \
+                      awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+                raw=`echo "scale=2;($(display_units ${free})*100)/$(display_units "${total}")"|bc`
+            else
+                raw=`echo "${output}" | grep -e "^Disk Space Free" | \
+                     awk -F: '{print $2}'|awk '{$1=$1};1'|sort -n|head -1`
+            fi
+        fi
+        res=$( display_units "${raw}" )
     fi
 elif [[ ${ATTR} == 'clients' ]]; then
     output=`${GLUSTER} volume status ${VOLNAME} clients 2>/dev/null`
